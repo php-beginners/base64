9a6bf22a42d70e5ec586e3bf2cd1b0380590bee5
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-04-10 18:56:06 +0900

    Fix: index.php | Ready to base64

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 	<!-- JavaScript -->
 	<script src="canvas.js?<?= date('H') ?>" defer></script>
+    <script src="base64.js?<?= date('H') ?>" defer></script>
 </head>
 <body>
 	<div class="margin">
@@ -16,6 +17,13 @@
 			This browser does not support canvas tags.
 		</canvas><br/>
 		<button id="clear">Clear</button>
+        <button id="base64">Base64</button>
+        <hr/>
+        <a   id="a"   href="">base64</a><br/>
+        <img id="img" src=""/>
+        <canvas id="carbon" width="800px" height="200px">
+            This browser does not support canvas tags.
+        </canvas><br/>
 	</div>
 </body>
 </html>
