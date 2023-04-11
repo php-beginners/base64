7bb3de55b019c3cf4160c72eb848704bfb25bbb0
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-04-11 11:47:11 +0900

    New: api.js

---
+++ api.js
@@ -0,0 +1,9 @@
+
+/** phpbeginners-base64:/api.js
+ *
+ * @created     2023-04-11
+ * @version     1.0
+ * @package     phpbeginners-base64
+ * @author      phpbeginners <phpbeginners@gmail.com>
+ * @copyright   phpbeginners All right reserved.
+ */
